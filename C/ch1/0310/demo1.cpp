fe16ee091276b74a60828c00acee3bf72fd6c5f2
haha39 <60254357+haha39@users.noreply.github.com>
2023-03-14 23:22:06 +0800

    Create demo1.cpp

---
+++ C/ch1/0310/demo1.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+//using namespace std;
+
+int main()
+{
+
+	int a = 9;
+	int b = a++;
+	printf("b = a++ : a = %d, b = %d\n", a, b);
+	
+	
+
+	
+	
+
+
+	system("PAUSE");
+	return 0;
+}
+
