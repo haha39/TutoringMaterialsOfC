cf3260ee24e2ed9207ba42101bfddcfc1d6720e9
haha39 <60254357+haha39@users.noreply.github.com>
2023-04-14 22:41:34 +0800

    Update app13_4.java

--- Java/hw6/app13_4.java
+++ Java/hw6/app13_4.java
@@ -1,16 +1,17 @@
 public class app13_4 {
-    public static void divide(int a, int b) throws AbstractMethodError {
+    public static int divide(int a, int b) throws AbstractMethodError {
         int c;
         c = a / b;
         System.out.println(a + "/" + b + "=" + c);
 
+        return c;
     }
 
     public static void main(String[] args) {
         int a = 4, b = 0;
 
         try {
-            divide(a, b);
+            int d = divide(a, b);
         } catch (ArithmeticException e) {
             System.out.println("exception = " + e);
         }
