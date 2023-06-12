586669fa230d607e9ea0eaa05bdf9aa80ba16970
haha39 <60254357+haha39@users.noreply.github.com>
2023-06-12 18:23:55 +0800

    Update hw1.c
    malloc 1D

--- ch6/hw1.c
+++ ch6/hw1.c
@@ -3,7 +3,26 @@
 
 int main()
 {
-
+	int *dynArr;
+	
+	int arrLen = 10;
+	
+	dynArr = malloc(arrLen * sizeof(int));
+	
+	if(dynArr == NULL) {
+		fprintf(stderr, "Error : unable to allocate required memory\n");
+		return 1;
+	}
+	
+	int i;
+	
+	for(i = 0; i < arrLen; i++) {
+		dynArr[i] = i;
+		printf("%d\n", dynArr[i]);
+	}
+	
+	free(dynArr);
+	
 	system("PAUSE");
 	return 0;
 }
