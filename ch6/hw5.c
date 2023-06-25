3602d502951e90f20fa88a1e0a0dd1b0bcf9b8a0
haha39 <60254357+haha39@users.noreply.github.com>
2023-06-25 14:33:29 +0800

    Update hw5.c

--- ch6/hw5.c
+++ ch6/hw5.c
@@ -1,10 +1,26 @@
-#include <>
-#include <>
-//using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-
+	int *dynArr;
+	int arrLen = 10;
+	
+	dynArr = malloc(arrLen * sizeof(int));
+	if( dynArr == NULL ) {
+		fprintf(stderr, "Error: unable to allocate required memory\n");
+		return 1;
+	}
+ 	
+	memset(dynArr, 100, arrLen * sizeof(int));
+	
+	int i;
+	for (i = 0; i < arrLen; i++) {
+		printf("%d\n", dynArr[i]);
+  	}
+  	
+  	free(dynArr);
 	system("PAUSE");
 	return 0;
 }
