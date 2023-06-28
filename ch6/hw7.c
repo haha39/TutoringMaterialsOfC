7253c5b27cb899ec43324ba0669e18b371fdb8f5
haha39 <60254357+haha39@users.noreply.github.com>
2023-06-28 19:48:45 +0800

    Update hw7.c

--- ch6/hw7.c
+++ ch6/hw7.c
@@ -3,6 +3,26 @@
 
 int main()
 {
+	int i, j;
+	int size = 5;
+	int **a = (int**)malloc(size * sizeof(int*));
+	
+	for(i = 0; i < size; i++) {
+		a[i] = (int*)malloc(size * sizeof(int));
+	}
+	
+	for(i = 0; i < size; i++) {
+		for(j = 0; j < size; j++) {
+			printf("%p	", &a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	for(i = 0; i < size; i++) {
+		free(a[i]);
+	}
+	
+	free(a);
 
 	system("PAUSE");
 	return 0;
