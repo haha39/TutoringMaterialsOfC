989bb332a718f223136f725d23336160382fc39c
haha39 <60254357+haha39@users.noreply.github.com>
2023-06-28 19:51:57 +0800

    Update hw8.c

--- ch6/hw8.c
+++ ch6/hw8.c
@@ -3,7 +3,18 @@
 
 int main()
 {
-
+	int i, j;
+	int size = 2;
+	int (*a)[size] = (int(*)[size])malloc(size * size * sizeof(int));
+	
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			printf("%p ", &a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	free(a);
 	system("PAUSE");
 	return 0;
 }
